1724ddbe1255f1e583c4444734cd9e6a997f151b
Anirudh <danirud4@gmail.com>
2026-01-19 22:23:01 +0530

    aded

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "hello from docker"
+    return "hello from dockedddr"
 
 if __name__=="__main__":
     app.run(host="0.0.0.0", port=5000)
